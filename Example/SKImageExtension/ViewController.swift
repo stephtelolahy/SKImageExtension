1c1de864826a711723e969b47fe392a654f1c6b9
Telolahy <stephano.telolahy@gmail.com>
2019-02-02 15:10:21 +0100

    Initial commit

---
+++ Example/SKImageExtension/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SKImageExtension
+//
+//  Created by Telolahy on 02/02/2019.
+//  Copyright (c) 2019 Telolahy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
